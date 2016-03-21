43f927449db6d3f0008e0dc4dd6daf192fc65086
Shayna Worthen <worthensf@gmail.com>
2016-03-21 01:39:20 -0700

    layout done, images uploaded and JS running

--- js/main.js
+++ js/main.js
@@ -3,41 +3,49 @@
 $(document).on('ready', function() {
     
     
-    
+    // TODO: Inside of your on ready handler, invoke the Leaflet.js library
+// to draw a map in your `#map-container` div.
+    var osmUrl='http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+    var osmAttrib='Map data © <a href="http://openstreetmap.org">OpenStreetMap</a> contributors';
+    var osm = new L.TileLayer(osmUrl, {minZoom: 8, maxZoom: 18, attribution: osmAttrib});
 
+// TODO: Add 2 layers to your map you have visuals. Use the Open Street Maps
+// tiles served through the MapQuest CDN. Consult this example to set up
+// the map tiles layers:
+
+    var satLayer = L.tileLayer( 'http://{s}.mqcdn.com/tiles/1.0.0/sat/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="http://osm.org/copyright" title="OpenStreetMap" target="_blank">OpenStreetMap</a> contributors | Tiles Courtesy of <a href="http://www.mapquest.com/" title="MapQuest" target="_blank">MapQuest</a> <img src="http://developer.mapquest.com/content/osm/mq_logo.png" width="16" height="16">',
+        subdomains: ['otile1','otile2','otile3','otile4']
+    });
+
+    var drawLayer = L.tileLayer( 'http://{s}.mqcdn.com/tiles/1.0.0/osm/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="http://osm.org/copyright" title="OpenStreetMap" target="_blank">OpenStreetMap</a> contributors | Tiles Courtesy of <a href="http://www.mapquest.com/" title="MapQuest" target="_blank">MapQuest</a> <img src="http://developer.mapquest.com/content/osm/mq_logo.png" width="16" height="16">',
+        subdomains: ['otile1','otile2','otile3','otile4']
+    });
+
+    var mapLayers = {
+        "Satellite": satLayer,
+        "Map View": drawLayer,
+        "Open Street Maps": osm
+    }
+
+    var map = L.map('map-container').setView([48.6542, -122.9383], 10 );
+
+    L.control.layers(mapLayers).addTo(map);
+    satLayer.addTo(map);
+
+// TODO: Customize that Map to show markers with popups at no fewer than 3
+// interesting locations. 
+
+    var marker = L.marker([48.6542, -122.9383]).addTo(map);
+    marker.bindPopup("<b>Orcas Island</b><br>Home of Camp Orkiss!");
     
-  //  Define a validation object for use on your page:
-    $( "#request-info" ).validate({
-        // 3. Connect the validation object to an event handler tied to the submit button:
-         submitHandler: function(form) {
-            // If form is valid, submit it!
-            form.submit();
-        },
-        
-        rules: {
-            "your-name": {
-                required: true,
-                maxlength: 128,
-                minlength: 2,
-                lettersonly: true //used method created below so it may not contain numbers
-            },
-            "your-email": {
-                required: true
-            }
-            
-        }// End rules
-    });// End validate function
+    var marker = L.marker([48.6967, -122.9042]).addTo(map);
+    marker.bindPopup("<b>Eastsound, WA</b>");
     
-        //Create method that only allows letters and spaces, no numbers:
-    jQuery.validator.addMethod("lettersonly", function(value, element) {
-        return this.optional(element) || /^[a-z\s]+$/i.test(value);
-    }, "Letters only please")
-           
-           //alert tied to submit button click.
-  $('#submit').on('click', function() {
-     $('#success').show();
-       });
-           
+    var marker = L.marker([48.6776, -122.8312]).addTo(map);
+    marker.bindPopup("<b>Mt. Constitution</b><br>Moran State Park");
+    
      // Bootstrap Tooltips
     $('label span.glyphicon').tooltip();
     
---
+++ js/work-at-camp.js
@@ -0,0 +1,31 @@
+
+// ready handler
+$(document).on('ready', function() {
+    
+//Create method that only allows letters and spaces, no numbers:
+    jQuery.validator.addMethod("lettersonly", function(value, element) {
+        return this.optional(element) || /^[a-z\s]+$/i.test(value);
+    }, "Letters only please")
+    
+  //  Define a validation object for use on your page & 
+  //  connect the validation object to an event handler tied to the submit button::
+    $( "#request-info" ).validate({
+         submitHandler: function(form) {
+            // If form is valid, submit it!
+            form.submit();
+        },
+        
+        rules: {
+            "your-name": {
+                required: true,
+                maxlength: 128,
+                minlength: 2,
+                lettersonly: true //used method created above so it may not contain numbers
+            },
+            "your-email": {
+                required: true
+            }
+            
+        }// End rules
+    });// End validate function
+}); // End .on ready function
